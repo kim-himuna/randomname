5c046e187ee33e08764c66c81ef7c69fe3b76d09
kim <kimurakimuna@gmail.com>
2022-01-02 02:15:49 +0900

    characterstringのバリデーション削除

--- src/main/java/example/domain/model/word/CharacterString.java
+++ src/main/java/example/domain/model/word/CharacterString.java
@@ -1,13 +1,7 @@
 package example.domain.model.word;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 public class CharacterString {
-    
-    final int maxrange = 10;
-    @NotBlank
-    @Size(max = maxrange, message = "{max}文字以内で入力してください。")
+
     private String value;
 
     public CharacterString(String value){
