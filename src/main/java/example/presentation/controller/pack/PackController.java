c1ca9292e52fd445e657465986d13b2108972463
kim <kimurakimuna@gmail.com>
2022-03-06 00:00:47 +0900

    likeメソッドを分離, パック表示をpacklist型へ

--- src/main/java/example/presentation/controller/pack/PackController.java
+++ src/main/java/example/presentation/controller/pack/PackController.java
@@ -1,5 +1,8 @@
 package example.presentation.controller.pack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -11,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import example.application.service.LikeService;
 import example.application.service.PackService;
 import example.application.service.UserAuthDetails;
-import example.domain.model.like.Like;
+import example.domain.model.Session;
 import example.domain.model.pack.*;
+import example.presentation.helper.PackToPackFormHelper;
 
 /**pack詳細 */
 
@@ -26,21 +30,24 @@ public class PackController {
     @Autowired
     LikeService likeService;
 
+    @Autowired
+    Session session;
+    @Autowired
+    PackToPackFormHelper toPackForm;
+
     @GetMapping
-    public String packDetail(Model model,@PathVariable(value = "packId") PackId packId){
+    public String packDetail(Model model,@PathVariable(value = "packId") PackId packId,@AuthenticationPrincipal UserAuthDetails userAuthDetails){
         Pack pack = packService.getPack(packId);
         if(pack == null){
             return "redirect:/top";
         }
-        model.addAttribute("pack",pack);
+
+        List<Pack> packList = new ArrayList<>();
+        packList.add(pack);
+        
+        model.addAttribute("pack",toPackForm.toPackFormList(packList, session, userAuthDetails, likeService).get(0));
         return "packs/packDetail";
     }
 
-    @GetMapping("like")
-    public String likePack(Model model,@AuthenticationPrincipal UserAuthDetails userAuthDetails,@PathVariable(value = "packId") PackId packId){
 
-        likeService.create(new Like(null, userAuthDetails.getUserId(), packId));
-
-        return "redirect:/top";
-    }
 }
---
+++ src/main/java/example/presentation/controller/pack/like/LikeController.java
@@ -0,0 +1,43 @@
+package example.presentation.controller.pack.like;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import example.application.service.LikeService;
+import example.application.service.UserAuthDetails;
+import example.domain.model.like.Like;
+import example.domain.model.pack.PackId;
+
+@Controller
+@RequestMapping("like/{packId}/{fromPage}")
+public class LikeController {
+
+    @Autowired
+    LikeService likeService;
+
+    @GetMapping
+    public String likePack(Model model,@AuthenticationPrincipal UserAuthDetails userAuthDetails,@PathVariable(value = "packId") PackId packId,@PathVariable(value = "fromPage") String fromPage){
+
+        likeService.create(new Like(null, userAuthDetails.getUserId(), packId));
+
+        if(fromPage.equals("top")){
+            return "redirect:/top";
+        }else if(fromPage.equals("packsDetail")){
+            return "redirect:/packs/detail/" + packId.getValue();
+        }else if(fromPage.equals("productlist")){
+            return "redirect:/users/product/from" + packId.getValue();
+        }else if(fromPage.equals("usersDetail")){
+            return "redirect:/users/detail";
+        }else if(fromPage.equals("shuffleDetail")){
+            return "redirect:/users/packs/shuffle/detail";
+        }
+
+        return "redirect:/top";
+    }
+    
+}
