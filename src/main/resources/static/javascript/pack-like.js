eb46d954d4e4d99cdfbcec97d676921b32947fc8
kim-himuna <80331805+kim-himuna@users.noreply.github.com>
2022-06-23 22:34:40 +0900

    お気に入り時の挙動を示すajax用js

---
+++ src/main/resources/static/javascript/pack-like.js
@@ -0,0 +1,20 @@
+'use strict';
+$(function(){
+    $("[id^='likebtn_']").click(function(){
+        var $this = $(this);
+        var packid = $this.data('packid');
+
+        $.ajax({
+            type:"POST",
+            url: "/like",
+            data: {
+                packid: $this.data("packid"),
+            }
+        }).done(function(note) {
+            $('#likebtn_'+packid).html(note);
+        }).fail(function() {
+            alert("通信に失敗しました。")
+        }).always(function() {
+        })
+    });
+});
